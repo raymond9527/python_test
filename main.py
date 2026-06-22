0781bc9f272e4198aba5364c6aed2ae6ed811c9d
detachment <1109203542@qq.com>
2026-06-22 11:33:14 +0200

    完成了第一个测试任务：简易计算器

--- main.py
+++
@@ -1,3 +0,0 @@
-print("learn python with me")
-print("project test")
-print("whats going on?")
---
+++ test1.py
@@ -0,0 +1,16 @@
+print("欢迎使用简单计算器！")
+keepgoing = "y"
+countlist = ["+", "-", "*", "/", "%", "//", "**"]
+while keepgoing == "y":
+    try:
+        test1 = float(input("请输入第一个数字："))
+        count1 = input("请输入运算方式（+，-，*，/，%，//，**）：")
+        if count1 in countlist:
+            test2 = float(input("请输入第二个数字："))
+            result = eval(test1+count1+test2)
+            print(f"计算的结果是：{result}")
+            keepgoing = input("是否继续?（输入y继续)：").lower()
+        else:
+            print("你是不是弱智？加减乘除都能输入错误！")
+    except ValueError:
+        print("请检查你输入的数字！")
