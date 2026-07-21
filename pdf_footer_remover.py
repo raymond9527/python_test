6276ee026b96b3cc231c2f92945c6c0bbec7e694
detachment <1109203542@qq.com>
2026-07-21 23:55:30 +0200

    升级了脚本，适用于生成可执行程序

--- pdf_footer_remover.py
+++ pdf_footer_remover.py
@@ -1,4 +1,26 @@
 import fitz
+import os
+import sys
+
+
+def get_base_path():
+    """
+    获取程序运行目录
+
+    兼容：
+    1. Python源码运行
+    2. PyInstaller打包exe运行
+    """
+
+    if getattr(sys, "frozen", False):
+        # exe运行
+        return os.path.dirname(sys.executable)
+
+    else:
+        # Python运行
+        return os.path.dirname(
+            os.path.abspath(__file__)
+        )
 
 
 def remove_pdf_footer(
@@ -11,9 +33,9 @@ def remove_pdf_footer(
 
     参数
     ----------
-    input_pdf : 输入PDF
+    input_pdf : 输入PDF路径
 
-    output_pdf : 输出PDF
+    output_pdf : 输出PDF路径
 
     footer_height : 页脚高度(pt)
     """
@@ -33,13 +55,13 @@ def remove_pdf_footer(
             height
         )
 
-        # 添加擦除标记
+        # 添加删除区域
         page.add_redact_annot(
             footer_rect,
-            fill=(1, 1, 1)      # 白色填充
+            fill=(1, 1, 1)
         )
 
-        # 真正删除对象
+        # 执行删除
         page.apply_redactions()
 
     doc.save(
@@ -51,12 +73,47 @@ def remove_pdf_footer(
     doc.close()
 
     print("处理完成！")
+    print(
+        f"输出文件：{output_pdf}"
+    )
 
 
 if __name__ == "__main__":
 
+    # 获取程序所在目录
+    base_path = get_base_path()
+
+    # 当前目录下的input.pdf
+    input_file = os.path.join(
+        base_path,
+        "input.pdf"
+    )
+
+    # 输出output.pdf
+    output_file = os.path.join(
+        base_path,
+        "output.pdf"
+    )
+
+    # 检查输入文件
+    if not os.path.exists(input_file):
+
+        print(
+            "错误：当前目录没有找到 input.pdf"
+        )
+
+        input(
+            "按Enter退出..."
+        )
+
+        sys.exit()
+
     remove_pdf_footer(
-        "pdf_footer_remover\input.pdf",
-        "pdf_footer_remover\output.pdf",
+        input_file,
+        output_file,
         footer_height=40
+    )
+
+    input(
+        "按Enter退出..."
     )
